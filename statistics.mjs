fedac111ab479a5d17b2775fe2578b1f2af565db
Rekha M <rem@Rekhas-MacBook-Pro.local>
2025-04-25 10:14:07 +0530

    main code

--- statistics.mjs
+++ statistics.mjs
@@ -2,4 +2,11 @@
 export function computeStatistics(numbers){
     
     // implement the computation of statistics here
+    const total = numbers.reduce((sum, num) => sum + num, 0);
+    const average = total / numbers.length;
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
+
+    return { average, min, max };
+
 }
